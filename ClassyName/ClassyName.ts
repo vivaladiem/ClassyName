6822ff230b0a10eaa2fdd1083485440bcb87b7a7
YoungWoo Im <stripleo91@gmail.com>
2022-04-11 19:31:50 +0900

    Add project files.

---
+++ ClassyName/ClassyName.ts
@@ -0,0 +1,57 @@
+//ClassyName.ts
+
+class ClassyName {
+    private result: string;
+    private base: string;
+    static elementDelimiter = "__";
+    static modifierDelimiter = "--";
+
+    /**
+     * 
+     * @param baseName : a block name or an block__element-name.
+     */
+    constructor(baseName: string) {
+        this.result = baseName;
+        this.base = baseName;
+    }
+
+    setElementDelimiter(delimiter: string) {
+        this.result = this.result.replace(new RegExp(ClassyName.elementDelimiter, "g"), delimiter);
+        ClassyName.elementDelimiter = delimiter;
+    }
+
+    setModifierDelimiter(delimiter: string) {
+        this.result = this.result.replace(new RegExp(ClassyName.modifierDelimiter, "g"), delimiter);
+        ClassyName.modifierDelimiter = delimiter;
+    }
+
+    element(element: string): ClassyName {
+        this.result += ClassyName.elementDelimiter + element;
+        this.base = this.result;
+
+        return this;
+    }
+
+    modifier(modifierName: string, modifierValue?: string): ClassyName {
+        this.result += " " + this.base + ClassyName.modifierDelimiter + modifierName;
+
+        if (modifierValue) {
+            this.result += ClassyName.modifierDelimiter + modifierValue;
+        }
+
+        return this;
+    }
+
+    externalClassName(className: string): ClassyName {
+        this.result += " " + className;
+
+        return this;
+    }
+
+    getResult(): string {
+        return this.result;
+    }
+
+}
+
+export default ClassyName;
---
+++ ClassyName/app.js
@@ -0,0 +1,25 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const ClassyName_1 = require("./ClassyName");
+console.log("클래시네임 테스트");
+console.log("InputForm 블록을 생성한다.");
+let classyName = new ClassyName_1.default("Input");
+console.log(classyName.getResult());
+console.log("엘리먼트를 지정한다.");
+classyName.element("Label");
+console.log(classyName.getResult());
+console.log("modifier를 지정한다");
+classyName.modifier("green");
+console.log(classyName.getResult());
+console.log("modifier와 그 값을 지정한다.");
+classyName.modifier("size", "l");
+console.log(classyName.getResult());
+console.log("element delimiter를 변경한다");
+classyName.setElementDelimiter("____");
+console.log(classyName.getResult());
+console.log("modifier delimiter를 변경한다");
+classyName.setModifierDelimiter("_");
+console.log(classyName.getResult());
+console.log("종료하려면 아무거나 입력 후 엔터를 누르세요.");
+process.stdin.read(0); // 꺼짐 방지
+//# sourceMappingURL=app.js.map
